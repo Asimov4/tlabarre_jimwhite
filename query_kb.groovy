227252e6202063e929637d1f4beab7d6b854b67f
Jim White <james.paul.white@gmail.com>
2012-04-06 16:21:53 -0700

    Query tweaks.

--- query_kb.groovy
+++ query_kb.groovy
@@ -12,11 +12,9 @@ import org.apache.lucene.search.Query
 
 indexDirectory = FSDirectory.open(new File("index_kb"))
 
-field = "contents"
-
 searcher = new IndexSearcher(indexDirectory)
 analyzer = new StandardAnalyzer(Version.LUCENE_CURRENT)
-parser = new QueryParser(Version.LUCENE_CURRENT, field, analyzer)
+parser = new QueryParser(Version.LUCENE_CURRENT, "contents", analyzer)
 
 args = args as List
 
@@ -32,43 +30,17 @@ def do_query(Query query)
 {
     println query
 
-    hits = searcher.search(query, 10)
+    hits = searcher.search(query, 5)
 
     println "${hits.totalHits} hits"
-
+    
     for (hit in hits.scoreDocs) {
+	    println()
+
         def doc = searcher.doc(hit.doc)
         println(doc.get("path"))
-        println doc.get("line_number")
-        println doc.get("line_count")
+//        println doc.get("line_number")
+//        println doc.get("line_count")
         doc.fields.each { println it }
-        println()
     }
 }
-
-//args.each { arg ->
-//    switch (arg) {
-//        case '-id' : field = 'entity_id'
-//            break
-//        case '-type' : field = 'entity_type'
-//            break
-//        case '-name' : field = 'entity_name'
-//            break
-//        case '-title' : field = 'entity_title'
-//            break
-//
-//        default:
-//            query = parser.parse(args[0])
-//
-//            hits = searcher.search(query, 100)
-//
-//            for (hit in hits.scoreDocs) {
-//                def doc = searcher.doc(hit.doc)
-//                println( doc.get("path") )
-//                println doc.get("line_number")
-//                println doc.get("line_count")
-//                doc.fields.each { println it }
-//                println()
-//            }
-//    }
-//}
